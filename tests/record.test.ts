3255eb5204d3963cd4cf19970d728b4ba53187a6
Dev-Dolawatta <.dtdolawatta@gmail.com>
2026-05-30 20:24:59 +0530

    record some user  actions

---
+++ tests/record.test.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://ecommerce-playground.lambdatest.io/index.php?route=common/home');
+  await page.getByRole('button', { name: ' My account' }).click();
+  await page.getByRole('paragraph').filter({ hasText: 'This is a dummy website for' }).click();
+  await page.getByRole('textbox', { name: 'E-Mail Address' }).click();
+  await page.getByRole('textbox', { name: 'E-Mail Address' }).fill('dt@gmail.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('12345678');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('link', { name: ' Edit your account' }).click();
+  await page.getByRole('textbox', { name: 'First Name *' }).click();
+  await page.getByRole('textbox', { name: 'First Name *' }).fill('Dolawaththa ');
+  await page.getByRole('button', { name: 'Continue' }).click();
+});
