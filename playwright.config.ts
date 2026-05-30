9ff297ae07f7a6b727ae28a8db65a5ffdd9b3862
Dev-Dolawatta <.dtdolawatta@gmail.com>
2026-05-30 21:21:21 +0530

    report generation

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,11 @@ const config: PlaywrightTestConfig = {
   testMatch:["tests/record.test.ts"],// Specify the test files to run, in this case only the Login.test.ts file
   use:{
     headless : false
-  }
+  },
+  //reporter configuration to generate reports in different formats
+  reporter:[["dot"],//GREEN if right and RED if wrong
+  ["json",{outputFile:"jsonReports/report.json"}],//generate a json report in the specified path
+  ["html",{open:"never"}]]//generate an html report in the specified path and open it manually when needed
 };
 // Look for test files in the "tests" directory, relative to this configuration file.
 
