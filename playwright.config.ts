00ebb4a3748ba7a895f593060e13f209336000f0
Dev-Dolawatta <.dtdolawatta@gmail.com>
2026-05-30 20:31:20 +0530

    confirm test records the user action by running test record

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,11 @@
 import type { PlaywrightTestConfig } from "playwright/test";// See https://playwright.dev/docs/test-configuration.
 
 const config: PlaywrightTestConfig = {
-  testMatch:["tests/Login.test.ts"] // Specify the test files to run, in this case only the Login.test.ts file
-}
+  testMatch:["tests/record.test.ts"],// Specify the test files to run, in this case only the Login.test.ts file
+  use:{
+    headless : false
+  }
+};
 // Look for test files in the "tests" directory, relative to this configuration file.
 
 export default config;
--- tests/record.test.ts
+++ tests/record.test.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://ecommerce-playground.lambdatest.io/index.php?route=common/home');
+  await page.hover('//*[@id="widget-navbar-217834"]/ul/li[6]/a/div/span')//since test recorder cant record every action need to say to hover before click
   await page.getByRole('button', { name: ' My account' }).click();
   await page.getByRole('paragraph').filter({ hasText: 'This is a dummy website for' }).click();
   await page.getByRole('textbox', { name: 'E-Mail Address' }).click();
